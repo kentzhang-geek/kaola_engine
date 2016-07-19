32678fc883a5ab0497a3012443d4cc14eaf4dbf8
Kent Zhang <Kent Zhang>
2016-07-19 14:10:09 +0800

    now finish hdr test and turn to light system

--- src/render/render_addons/post_processer_1.cpp
+++ src/render/render_addons/post_processer_1.cpp
@@ -3,66 +3,35 @@
 GL3D_POST_PROCESS_IMPLEMENT(post_process_gauss);
 GL3D_POST_PROCESS_IMPLEMENT(hdr_test);
 
-//#include "kaola_engine/gl3d_post_process.h"
-//#include "kaola_engine/gl3d.hpp"
-//#include "kaola_engine/shader_manager.hpp"
-
-//using namespace gl3d;
-
-//static gl3d::obj_points post_rect[4] = {
-//    {-1.0, 1.0, 0.0,  // vt
-//     0.0, 0.0, 0.0,   // normal
-//     0.0, 0.0, 0.0, 0.0, // color
-//        0.0, 1.0,     // text
-//        0},
-//    {-1.0, -1.0, 0.0,  // vt
-//     0.0, 0.0, 0.0,   // normal
-//     0.0, 0.0, 0.0, 0.0, // color
-//        0.0, 0.0,     // text
-//        0},
-//    {1.0, 1.0, 0.0,  // vt
-//     0.0, 0.0, 0.0,   // normal
-//     0.0, 0.0, 0.0, 0.0, // color
-//        1.0, 1.0,     // text
-//        0},
-//    {1.0, -1.0, 0.0,  // vt
-//     0.0, 0.0, 0.0,   // normal
-//     0.0, 0.0, 0.0, 0.0, // color
-//        1.0, 0.0,     // text
-//        0},
-//};
-
-//static GLushort post_indexes[6] = {
-//    0, 2, 1,
-//    1, 2, 3,
-//};
-
-//class special_obj : public gl3d::object {
+//class hdr_test : public gl3d_post_processer {
 //public:
-//    special_obj(gl3d_general_texture * p_tex) : object(post_rect, 4, post_indexes, 6) {
-//        gl3d_material * p_mat = new gl3d_material(p_tex);
-//        this->get_meshes()->at(0)->set_bounding_value_max(glm::vec3(1.0, 1.0, 0.0));
-//        this->get_meshes()->at(0)->set_bounding_value_max(glm::vec3(-1.0, -1.0, 0.0));
-//        this->get_meshes()->at(0)->set_material_index(0);
-//        this->get_property()->authority = GL3D_OBJ_ENABLE_DEL;
-//        this->get_mtls()->insert(0, p_mat);
-//        this->get_property()->draw_authority = GL3D_SCENE_DRAW_SPECIAL;
+//    float get_lum(GLubyte * pixs) {
+//        return 0.0;
 //    }
-//};
 
-//class copy_only : public gl3d_post_processer {
-//public:
-//    gl3d_general_texture * process(gl3d_general_texture *src, scene *scene) {
-//        return src;
-//    }
-//    void deel_with_src(gl3d_general_texture * src) {};
-//};
-//GL3D_ADD_POST_PROCESSER(copy_only);
-
-//class gauss : public gl3d_post_processer {
-//public:
-//    gl3d_general_texture * process(gl3d_general_texture *src, scene *one_scene) {
+//    gl3d_general_texture * process(gl3d_general_texture *src, scene *one_scene){
 //        special_obj * sobj = new special_obj(src);
+
+//        // get lum_max and lum_min
+//        GLubyte pix[4];
+//        const int steps = 16;
+//        float lum_max = 0.0;
+//        float lum_min = 1.0;
+//        gl3d::gl3d_framebuffer tmp_fb(GL3D_FRAME_HAS_DEPTH_BUF | GL3D_FRAME_HAS_STENCIL_BUF,
+//                                      src->get_size_x(),
+//                                      src->get_size_y());
+//        tmp_fb.attach_color_text(src->get_text_obj());
+//        tmp_fb.use_this_frame();
+//        float lum;
+//        for (int x = 0; x < steps; x++) {
+//            for (int y = 0; y < steps; y++) {
+//                glReadPixels(x / steps * src->get_size_x(),
+//                             y / steps * src->get_size_y(),
+//                             1, 1, GL_RGBA, GL_UNSIGNED_BYTE, pix);
+//                lum = this->get_lum(pix);
+//            }
+//        }
+
 //        gl3d_general_texture * dst = new gl3d_general_texture(src->get_type(),
 //                                                              src->get_size_x(),
 //                                                              src->get_size_y());
@@ -70,42 +39,36 @@ GL3D_POST_PROCESS_IMPLEMENT(hdr_test);
 //                                  src->get_size_x(),
 //                                  src->get_size_y());
 //        fb.attach_color_text(dst->get_text_obj());
-
-//        gl3d::shader_param * current_shader_param = GL3D_GET_PARAM("post_process_gauss");
+//        gl3d::shader_param * current_shader_param = ::gl3d::shader_manager::sharedInstance()->get_param("hdr_test");
 //        current_shader_param->user_data.insert(string("scene"), one_scene);
 //        one_scene->get_property()->current_draw_authority = GL3D_SCENE_DRAW_SPECIAL;
-//        one_scene->get_property()->global_shader = string("post_process_gauss");
+//        one_scene->get_property()->global_shader = "hdr_test";
 //        one_scene->add_obj(QPair<int, object *>(2333, sobj));
 //        one_scene->prepare_canvas(true);
 //        glDisable(GL_CULL_FACE);
 //        one_scene->draw(true);
 //        current_shader_param->user_data.erase(current_shader_param->user_data.find(string("scene")));
-
 //        one_scene->delete_obj(2333);
 //        delete sobj;
-
 //        return dst;
-//    }
-//    void deel_with_src(gl3d_general_texture * src) {};
+//    } ;
+//    void  deel_with_src(gl3d_general_texture * src) {};
 //};
-//GL3D_ADD_POST_PROCESSER(gauss);
-//GL3D_LOAD_SHADER(post_process_gauss,
-//                 post_process_gauss.vdata,
-//                 post_process_gauss.fdata);
+//GL3D_ADD_POST_PROCESSER(hdr_test);
+//GL3D_LOAD_SHADER(hdr_test, hdr_test.vdata, hdr_test.fdata);
 
-//GL3D_SHADER_PARAM(post_process_gauss) {
-//    GLuint pro = GL3D_GET_SHADER("post_process_gauss")->getProgramID();
+//GL3D_SHADER_PARAM(hdr_test) {
+//    GLuint pro = GL3D_GET_SHADER("hdr_test")->getProgramID();
 //    gl3d::object * obj = GL3D_GET_OBJ();
 //    glUniform1f(glGetUniformLocation(pro, "mtlSpecularExponent"), 0.3);
 //    glUniform1f(glGetUniformLocation(pro, "shininess"), 0.5);
-
-//    // 原色，散射与镜面分别是0，1，2
 //    glUniform1i(glGetUniformLocation(pro, "gl3d_texture_ambient"), 0);
 //    glUniform1i(glGetUniformLocation(pro, "gl3d_texture_diffuse"), 1);
 //    glUniform1i(glGetUniformLocation(pro, "gl3d_texture_specular"), 2);
-
 //    glm::vec3 light = glm::vec3(0.0, 1.0, -1.0);
 //    GL3D_SET_VEC3(light_vector, light, pro);
-
+//    glm::vec2 canvas_size(2.0 * gl3d_global_param::shared_instance()->canvas_width, 2.0 * gl3d_global_param::shared_instance()->canvas_height);
+//    glUniform2fv(glGetUniformLocation(pro, "canvas_size"), 1, glm::value_ptr(canvas_size));
 //    return true;
 //}
+
