6d44780eb8322793abf488634438f7226fdf8043
Kent Zhang <Kent Zhang>
2016-07-20 13:31:22 +0800

    will add a new branch

--- src/render/light_system/simple_directional_light.cpp
+++ src/render/light_system/simple_directional_light.cpp
@@ -9,7 +9,7 @@ using namespace gl3d;
 class simple_directional_light : public gl3d_post_processer {
 public:
     gl3d_general_texture *rend_light_pic(gl3d_general_texture *src, scene *scene);
-    gl3d_general_texture *compose_pic(gl3d_general_texture *src, scene *scene);
+    gl3d_general_texture *compose_pic(gl3d_general_texture *src, gl3d_general_texture *light, scene *scene);
     gl3d_general_texture * process(gl3d_general_texture *src, scene *scene);
     void deel_with_src(gl3d::gl3d_general_texture * src) {};
 };
@@ -19,7 +19,7 @@ gl3d_general_texture * simple_directional_light::process(
         gl3d_general_texture *src, scene *scene) {
     gl3d_general_texture * lr =
             this->rend_light_pic(src, scene);
-    return this->compose_pic(lr, scene);
+    return this->compose_pic(src, lr, scene);
 }
 
 gl3d_general_texture * simple_directional_light::rend_light_pic(
@@ -61,9 +61,15 @@ gl3d_general_texture * simple_directional_light::rend_light_pic(
 }
 
 gl3d_general_texture * simple_directional_light::compose_pic(
-        gl3d_general_texture *src, scene *scene) {
+        gl3d_general_texture *src,
+        gl3d_general_texture * light,
+        scene *scene) {
     // 准备将光照合成到最终呈现
     special_obj * sobj = new special_obj(src);
+    // 设置光照成果图
+    sobj->get_mtls()->value(
+                sobj->get_meshes()->at(0)->get_material_index()
+                )->textures.insert(gl3d_material::specular, light);
     gl3d_general_texture * dst = new gl3d_general_texture(src->get_type(),
                                                           src->get_size_x(),
                                                           src->get_size_y());
@@ -88,9 +94,9 @@ gl3d_general_texture * simple_directional_light::compose_pic(
     return dst;
 }
 
-GL3D_LOAD_SHADER(simple_directional_light,
-                 simple_directional_light.vdata,
-                 simple_directional_light.fdata);
+//GL3D_LOAD_SHADER(simple_directional_light,
+//                 simple_directional_light.vdata,
+//                 simple_directional_light.fdata);
 GL3D_SHADER_PARAM(simple_directional_light) {
     GLuint pro = GL3D_GET_SHADER("simple_directional_light")->getProgramID();
     gl3d::object * obj = GL3D_GET_OBJ();
@@ -104,9 +110,9 @@ GL3D_SHADER_PARAM(simple_directional_light) {
     return true;
 
 }
-GL3D_LOAD_SHADER(light_compose,
-                 light_compose.vdata,
-                 light_compose.fdata);
+//GL3D_LOAD_SHADER(light_compose,
+//                 light_compose.vdata,
+//                 light_compose.fdata);
 GL3D_SHADER_PARAM(light_compose) {
     GLuint pro = GL3D_GET_SHADER("light_compose")->getProgramID();
     gl3d::object * obj = GL3D_GET_OBJ();
@@ -118,5 +124,4 @@ GL3D_SHADER_PARAM(light_compose) {
     glm::vec3 light = glm::vec3(0.0, 1.0, -1.0);
     GL3D_SET_VEC3(light_vector, light, pro);
     return true;
-
 }
