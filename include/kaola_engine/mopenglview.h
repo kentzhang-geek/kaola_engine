854e1e4d88863e05cf86477dd0127b8aedc3044c
yananLi <q@q.com>
2016-08-05 15:22:04 +0800

    commit ui 20160805-3

--- include/kaola_engine/mopenglview.h
+++ include/kaola_engine/mopenglview.h
@@ -94,6 +94,8 @@ private:
     float drawhome_x1, drawhome_x2, drawhome_y1, drawhome_y2;
     QLabel *connectDot;
     QPair<gl3d_wall *, gl3d_wall *> combine_wall_pair;
+    glm::vec2 drawwall_start_point;
+    bool is_drawwall_start_point;
 
 
 
